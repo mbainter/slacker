6f4f10a291364dbaca497f8244fe0e0131be54e6
Mark Bainter <mark.bainter@messagesystems.com>
2015-04-06 22:05:29 -0500

    Enhancing the table flips

--- handlers/tableflip.go
+++ handlers/tableflip.go
@@ -1,6 +1,10 @@
 package handlers
 
-import sl "github.com/mbainter/slacker/slackerlib"
+import (
+    "time"
+    "math/rand"
+    sl "github.com/mbainter/slacker/slackerlib"
+)
 
 var Tableflip = sl.MessageHandler{
 	Name: `Tableflip`,
@@ -8,6 +12,18 @@ var Tableflip = sl.MessageHandler{
 	Method:  `HEAR`,
 	Pattern: `(?i)(table)*flip(table)*`,
 	Run: func(e *sl.Event, match []string) {
-		e.Respond(`(╯°□°）╯︵ ┻━┻`)
+		flips := []string{
+            "(╯°□°）╯︵ ┻━┻`",
+            "(╯°□°）╯︵ ┻━┻'",
+            "(ノ ゜Д゜)ノ ︵ ┻━┻'",
+            "(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)",
+            "(ノ^_^)ノ┻━┻ ┬─┬ ノ( ^_^ノ)",
+            "(╯°Д°）╯︵ /(.□ . \\)",
+            "(╯'□')╯︵ ┻━┻",
+            "(ﾉಥДಥ)ﾉ︵┻━┻･/",
+        }
+	 	now:=time.Now()
+	    rand.Seed(int64(now.Unix()))
+	    e.Respond(flips[rand.Intn(len(flips)-1)])
 	},
 }
